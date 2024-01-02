e38ebf9c855b43555bf8e9064400bc6a0d8238ac
zebleya1 <zebleya1@gmail.com>
2024-01-02 12:24:50 -0700

    added ways to find users by tags

--- backend/src/main/java/org/wecancodeit/backend/repositories/UserRepository.java
+++ backend/src/main/java/org/wecancodeit/backend/repositories/UserRepository.java
@@ -4,11 +4,12 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import org.wecancodeit.backend.models.User;
 
+import java.util.List;
 import java.util.Optional;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
-    
+
     /**
      * Finds a user by username.
      *
@@ -24,4 +25,16 @@ public interface UserRepository extends JpaRepository<User, Long> {
      * @return an Optional possibly containing the found user
      */
     Optional<User> findByEmail(String email);
+
+    /**
+     * finds users by similar User tags
+     */ 
+    default List<User> findBySimilarTags(String username, List<String> userTags) {
+        return findByMusicTagsInAndUsernameNot(userTags, username);
+    }
+
+    /*
+     * Finds users with similar music tags.
+     */
+    List<User> findByMusicTagsInAndUsernameNot(List<String> musicTags, String username);
 }
