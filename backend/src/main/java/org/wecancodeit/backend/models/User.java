4fb6e6bd149927b3e79aba4bf2c61c600d846879
zebleya1 <zebleya1@gmail.com>
2024-01-02 12:24:19 -0700

    added musictags getter & table

--- backend/src/main/java/org/wecancodeit/backend/models/User.java
+++ backend/src/main/java/org/wecancodeit/backend/models/User.java
@@ -1,6 +1,7 @@
 package org.wecancodeit.backend.models;
 
 import jakarta.persistence.*;
+import java.util.List;
 import java.util.Objects;
 
 /**
@@ -22,6 +23,9 @@ public class User {
     private String genre;
     private int experienceLevel;
     private String imageURL;
+
+    @ElementCollection
+    private List<String> musicTags;
 
     /**
      * Default constructor.
@@ -81,6 +85,10 @@ public class User {
 
     public String getImageURL() {
         return imageURL;
+    }
+    
+    public List<String> getMusicTags() {
+        return musicTags;
     }
 
     // Setters
