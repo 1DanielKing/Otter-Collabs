a7eaf37e215fc8e162d49d97f7d5753452614f29
zebleya1 <zebleya1@gmail.com>
2024-01-02 12:25:23 -0700

    added proper user import

--- backend/src/main/java/org/wecancodeit/backend/services/RecommendationService.java
+++ backend/src/main/java/org/wecancodeit/backend/services/RecommendationService.java
@@ -1,10 +1,11 @@
 package org.wecancodeit.backend.services;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.autoconfigure.security.SecurityProperties.User;
 import org.springframework.stereotype.Service;
+import org.wecancodeit.backend.models.User;
 import org.wecancodeit.backend.repositories.UserRepository;
 
 @Service
@@ -14,13 +15,10 @@ public class RecommendationService {
     private UserRepository userRepository;
 
     public List<User> getRecommendations(String username) {
-        // Start of Recommendation Algorithm
         Optional<User> currentUser = userRepository.findByUsername(username);
-
         if (currentUser.isPresent()) {
             User user = currentUser.get();
             List<String> userTags = user.getMusicTags();
-
             // This is just returning people with similar tags so beef it up
             return userRepository.findBySimilarTags(username, userTags);
         } else {
