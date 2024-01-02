b9733533ced3c1383c8657187b30434b162efca6
zebleya1 <zebleya1@gmail.com>
2024-01-02 11:22:01 -0700

    updated from main

--- backend/src/main/java/org/wecancodeit/backend/models/User.java
+++ backend/src/main/java/org/wecancodeit/backend/models/User.java
@@ -1,49 +1,152 @@
 package org.wecancodeit.backend.models;
 
-import org.springframework.stereotype.Indexed;
+import jakarta.persistence.*;
+import java.util.Objects;
 
+/**
+ * Represents a user in the OtterCollab platform.
+ * This class holds user details relevant for music collaboration.
+ */
 @Entity
+@Table(name = "app_users")
 public class User {
 
     @Id
-    @GeneratedValue(strategy = Generation.Type.IDENTITY)
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
+    
     private String username;
-    private String Email;
+    private String password;
+    private String email;
+    private String instrument;
+    private String genre;
+    private int experienceLevel;
+    private String imageURL;
 
-    @ElementCollection
-    private List<String> musicTags;
+    /**
+     * Default constructor.
+     */
+    public User() {
+    }
+
+    /**
+     * Constructs a new User with specified details.
+     *
+     * @param username
+     * @param password
+     * @param email
+     * @param instrument
+     * @param genre
+     * @param experienceLevel
+     * @param imageURL
+     */
+    public User(String username, String password, String email, String instrument, String genre, int experienceLevel,
+            String imageURL) {
+        this.username = username;
+        this.password = password;
+        this.email = email;
+        this.instrument = instrument;
+        this.genre = genre;
+        this.experienceLevel = experienceLevel;
+        this.imageURL = imageURL;
+    }
 
     public Long getId() {
         return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
     }
 
     public String getUsername() {
         return username;
     }
 
+    public String getPassword() {
+        return password;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getInstrument() {
+        return instrument;
+    }
+
+    public String getGenre() {
+        return genre;
+    }
+
+    public int getExperienceLevel() {
+        return experienceLevel;
+    }
+
+    public String getImageURL() {
+        return imageURL;
+    }
+
+    // Setters
+    public void setId(Long id) {
+        this.id = id;
+    }
+
     public void setUsername(String username) {
         this.username = username;
     }
 
-    public String getEmail() {
-        return Email;
+    public void setPassword(String password) {
+        this.password = password;
     }
 
     public void setEmail(String email) {
-        Email = email;
+        this.email = email;
     }
 
-    public List<String> getMusicTags() {
-        return musicTags;
+    public void setInstrument(String instrument) {
+        this.instrument = instrument;
     }
 
-    public void setMusicTags(List<String> musicTags) {
-        this.musicTags = musicTags;
+    public void setGenre(String genre) {
+        this.genre = genre;
     }
 
+    public void setExperienceLevel(int experienceLevel) {
+        this.experienceLevel = experienceLevel;
+    }
+
+    public void setImageURL(String imageURL) {
+        this.imageURL = imageURL;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o)
+            return true;
+        if (o == null || getClass() != o.getClass())
+            return false;
+        User user = (User) o;
+        return experienceLevel == user.experienceLevel &&
+                Objects.equals(id, user.id) &&
+                Objects.equals(username, user.username) &&
+                Objects.equals(email, user.email) &&
+                Objects.equals(instrument, user.instrument) &&
+                Objects.equals(genre, user.genre) &&
+                Objects.equals(imageURL, user.imageURL);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, username, email, instrument, genre, experienceLevel, imageURL);
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "id=" + id +
+                ", username='" + username + '\'' +
+                ", email='" + email + '\'' +
+                ", instrument='" + instrument + '\'' +
+                ", genre='" + genre + '\'' +
+                ", experienceLevel=" + experienceLevel +
+                ", imageURL='" + imageURL + '\'' +
+                '}';
+    }
 }
