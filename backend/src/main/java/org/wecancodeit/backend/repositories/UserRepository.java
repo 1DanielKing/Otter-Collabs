e4fb98d1050e4edec5dd8d587248cadcd9ff723f
zebleya1 <zebleya1@gmail.com>
2024-01-02 11:37:05 -0700

    updated from main

--- backend/src/main/java/org/wecancodeit/backend/repositories/UserRepository.java
+++ backend/src/main/java/org/wecancodeit/backend/repositories/UserRepository.java
@@ -1,8 +1,27 @@
 package org.wecancodeit.backend.repositories;
 
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import org.wecancodeit.backend.models.User;
+
+import java.util.Optional;
+
+@Repository
 public interface UserRepository extends JpaRepository<User, Long> {
+    
+    /**
+     * Finds a user by username.
+     *
+     * @param username
+     * @return an Optional possibly containing the found user
+     */
     Optional<User> findByUsername(String username);
 
-    @Query("SELECT u FROM User u WHERE u.username <> :username AND u.musicTags IN :userTags")
-    List<User> findBySimilarTags(@Param("username") String username, @Param("userTags")List<String> userTags);
+    /**
+     * Finds a user by email.
+     *
+     * @param email
+     * @return an Optional possibly containing the found user
+     */
+    Optional<User> findByEmail(String email);
 }
