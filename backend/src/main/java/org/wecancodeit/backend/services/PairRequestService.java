56dc35477a4b6676c720309f8862382de1860f87
zebleya1 <zebleya1@gmail.com>
2024-01-03 11:44:30 -0700

    finished some logic on service now need repo

--- backend/src/main/java/org/wecancodeit/backend/services/PairRequestService.java
+++ backend/src/main/java/org/wecancodeit/backend/services/PairRequestService.java
@@ -2,6 +2,8 @@ package org.wecancodeit.backend.services;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.wecancodeit.backend.models.PairRequest;
+import org.wecancodeit.backend.models.PairRequest.RequestStatus;
 import org.wecancodeit.backend.models.User;
 import org.wecancodeit.backend.repositories.UserRepository;
 
@@ -16,9 +18,22 @@ public class PairRequestService {
 
     public void sendPairRequest(String senderUsername, String receiverUsername, String message){
         //Find The members of the attempted pairing in the Database
-        User sender = userRepository.findByUsername(senderUsername).orElseThrow(() -> new IllegalArgumentException("Receiver not found")); 
-  
+        User sender = userRepository.findByUsername(senderUsername).orElseThrow(() -> new IllegalArgumentException("Sender not found")); 
+        User receiver = userRepository.findByUsername(receiverUsername).orElseThrow(() -> new IllegalArgumentException("Receiver not found")); 
+        
+        //New pair request instance
+        PairRequest pairRequest = new PairRequest();
+        pairRequest.setSender(sender);
+        pairRequest.setReceiver(receiver);
+        pairRequest.setMessage(message);
+
+         // Defaults to pending
+        pairRequest.setRequestStatus(RequestStatus.PENDING);
+
+        // Save the new PairRequest instance to the database
+        pairRequestRepository.save(pairRequest);
+    }
 
     }
     
-}
+
