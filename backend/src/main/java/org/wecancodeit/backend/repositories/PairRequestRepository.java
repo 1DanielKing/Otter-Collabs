56910ef529e45ad52901a361e6739996e8f70072
zebleya1 <zebleya1@gmail.com>
2024-01-03 12:57:25 -0700

    repository creation for responses

---
+++ backend/src/main/java/org/wecancodeit/backend/repositories/PairRequestRepository.java
@@ -0,0 +1,24 @@
+package org.wecancodeit.backend.repositories;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import org.wecancodeit.backend.models.PairRequest;
+import org.wecancodeit.backend.models.User;
+
+@Repository
+// Queries
+public interface PairRequestRepository extends JpaRepository<PairRequest, Long> {
+
+    // Find based on receiver and request status
+    List<PairRequest> findByReceiverAndRequestStatus(User receiver, PairRequest.RequestStatus requestStatus);
+
+    // Find by sender
+    List<PairRequest> findBySender_Username(String senderUsername);
+
+    // Find by receiver
+    List<PairRequest> findByReceiver_Username(String receiverUsername);
+
+}
