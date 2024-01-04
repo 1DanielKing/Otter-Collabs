6d2036ee14379fcdd537c0a4aeceb9fb293e03a1
zebleya1 <zebleya1@gmail.com>
2024-01-04 13:14:28 -0700

    This update includes a check for video content type, throwing a 400 error if a video upload is attempted.

--- backend/src/main/java/org/wecancodeit/backend/services/AudioService.java
+++ backend/src/main/java/org/wecancodeit/backend/services/AudioService.java
@@ -119,18 +119,19 @@ public class AudioService {
             audioMetaDataRepository.deleteById(id);
         });
     }
-/**
- * 
- * @param filePath to the audio file
- * @return the duration of the audio file in seconds
- */
-    private Double getAudioFileDuration(String filePath){
-        try{
+
+    /**
+     * 
+     * @param filePath to the audio file
+     * @return the duration of the audio file in seconds
+     */
+    private Double getAudioFileDuration(String filePath) {
+        try {
             File file = new File(filePath);
             AudioInputStream audioInputStream = AudioSystem.getAudioInputStream(file);
             AudioFileFormat fileFormat = AudioSystem.getAudioFileFormat(audioInputStream);
             long microseconds = (long) fileFormat.properties().get("duration");
-            return microseconds / 1_000_000.0; //Converts Microseconds to seconds
+            return microseconds / 1_000_000.0; // Converts Microseconds to seconds
         } catch (Exception e) {
             e.printStackTrace();
             return null;
