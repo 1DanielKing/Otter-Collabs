195c98a6558f9ee85b95fca8754c353250fba69d
Omar Bonnet <omar.bonnet@gmail.com>
2024-01-05 14:40:01 -0500

    temporarily permit all endpoints for testing

--- backend/src/main/java/org/wecancodeit/backend/Security/SecurityConfig.java
+++ backend/src/main/java/org/wecancodeit/backend/Security/SecurityConfig.java
@@ -24,12 +24,11 @@ public class SecurityConfig {
                 .csrf(csrf -> csrf.disable()) // Disable CSRF protection
                 .addFilterBefore(jwtTokenFilter, UsernamePasswordAuthenticationFilter.class)
                 .authorizeHttpRequests(authz -> authz
-                        .requestMatchers("/h2-console/**").permitAll() // Permit H2 console access
+                        .requestMatchers("/**").permitAll() // Permit H2 console access
                         .requestMatchers("/api/auth/**").permitAll() // Permit all requests to auth endpoints
-                        .requestMatchers("/chat/**").permitAll() //
+                        .requestMatchers("/chat/chat").permitAll() //
                         .requestMatchers("/api/users/**").permitAll()
-                        .requestMatchers("/api/audio/**").permitAll()
-                        .anyRequest().permitAll() // Require auth for all other requests
+                        .anyRequest().authenticated() // Require auth for all other requests
                 )
                 .headers(headers -> headers // Set headers to allow frame options for H2 console
                         .frameOptions().sameOrigin());
