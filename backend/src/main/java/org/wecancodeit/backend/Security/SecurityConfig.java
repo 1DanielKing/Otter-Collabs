f8395e42ba52375b9a424971fa16cd05cc0214ea
Omar Bonnet <omar.bonnet@gmail.com>
2024-01-05 14:48:19 -0500

    more specific security access

--- backend/src/main/java/org/wecancodeit/backend/Security/SecurityConfig.java
+++ backend/src/main/java/org/wecancodeit/backend/Security/SecurityConfig.java
@@ -24,7 +24,7 @@ public class SecurityConfig {
                 .csrf(csrf -> csrf.disable()) // Disable CSRF protection
                 .addFilterBefore(jwtTokenFilter, UsernamePasswordAuthenticationFilter.class)
                 .authorizeHttpRequests(authz -> authz
-                        .requestMatchers("/**").permitAll() // Permit H2 console access
+                        .requestMatchers("/h2-console/**").permitAll() // Permit H2 console access
                         .requestMatchers("/api/auth/**").permitAll() // Permit all requests to auth endpoints
                         .requestMatchers("/chat/**").permitAll() //
                         .requestMatchers("/api/users/**").permitAll()
