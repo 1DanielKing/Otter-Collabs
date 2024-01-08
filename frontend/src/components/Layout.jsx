9d93f7a7ffdcedc249a84911a69b6a68a839a515
Omar Bonnet <omar.bonnet@gmail.com>
2024-01-08 10:43:48 -0500

    import navbar css

--- frontend/src/components/Layout.jsx
+++ frontend/src/components/Layout.jsx
@@ -1,24 +1,22 @@
-import React, { useState } from 'react';
-import ChatBox from './ChatBox';
-import Navbar from './Navbar';
-import LogoutModal from './LogOutModal';
+import React, { useState } from "react";
+import ChatBox from "./ChatBox";
+import Navbar from "./Navbar";
 
 const Layout = ({ children }) => {
-    const [isLogoutModalOpen, setIsLogoutModalOpen] = useState(false);
+  const [isLogoutModalOpen, setIsLogoutModalOpen] = useState(false);
 
-    const openLogoutModal = () => setIsLogoutModalOpen(true);
-    const closeLogoutModal = () => setIsLogoutModalOpen(false);
+  const openLogoutModal = () => setIsLogoutModalOpen(true);
+  const closeLogoutModal = () => setIsLogoutModalOpen(false);
 
-    return (
-        <>
-            <div className="layout">
-                <Navbar openLogoutModal={openLogoutModal} />
-                <LogoutModal isOpen={isLogoutModalOpen} onClose={closeLogoutModal} />
-                <main>{children}</main>
-                <ChatBox />
-            </div>
-        </>
-    );
+  return (
+    <>
+      <div className="layout">
+        <Navbar />
+        <main>{children}</main>
+        <ChatBox />
+      </div>
+    </>
+  );
 };
 
 export default Layout;
