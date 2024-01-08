fd59b240eb7311ce0424f8c4da622e7c89aa258a
Omar Bonnet <omar.bonnet@gmail.com>
2024-01-08 10:38:24 -0500

    importing Daniel's navbar

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,19 +1,17 @@
-import React from 'react';
 import './Navbar.css'; // Import your CSS file
 
-function Navbar({ openLogoutModal }) {
+function Navbar() {
     return (
         <nav className="navbar">
             <ul className="nav-links">
                 <div className="Logo-Home-Button">
                     <li><a href="../pages/HomePage"><img src='/' alt='LOGO OTTERCOLLAB'></img></a></li>
                 </div>
-                <div className="nav-links">
+                <div className="rest-of-nav">
                     <div><li><a href="#">Profile</a></li></div>
                     <div><li><a href="#">Feed</a></li></div>
                     <div><li><a href="#">About</a></li></div>
                 </div>
-                <div><li><a href="#" onClick={openLogoutModal}>Log Out</a></li></div>
             </ul>
         </nav>
     );
