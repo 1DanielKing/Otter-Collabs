4e6d0379ae69269c0a3da83649182ae9e175e658
Omar Bonnet <omar.bonnet@gmail.com>
2024-01-08 11:13:53 -0500

    added logout button to navbar, functional

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,17 +1,19 @@
+import React from 'react';
 import './Navbar.css'; // Import your CSS file
 
-function Navbar() {
+function Navbar({ openLogoutModal }) {
     return (
         <nav className="navbar">
             <ul className="nav-links">
                 <div className="Logo-Home-Button">
                     <li><a href="../pages/HomePage"><img src='/' alt='LOGO OTTERCOLLAB'></img></a></li>
                 </div>
-                <div className="rest-of-nav">
+                <div className="nav-links">
                     <div><li><a href="#">Profile</a></li></div>
                     <div><li><a href="#">Feed</a></li></div>
                     <div><li><a href="#">About</a></li></div>
                 </div>
+                <div><li><a href="#" onClick={openLogoutModal}>Log Out</a></li></div>
             </ul>
         </nav>
     );
