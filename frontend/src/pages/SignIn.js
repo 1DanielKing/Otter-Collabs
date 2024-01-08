d741fefc665e7360266292209cfe30b354cdca7b
Omar Bonnet <omar.bonnet@gmail.com>
2024-01-08 10:21:48 -0500

    login with username now, added useAuth functionality

--- frontend/src/pages/SignIn.js
+++ frontend/src/pages/SignIn.js
@@ -1,52 +1,72 @@
 import React, { useState } from 'react';
-import { Link } from 'react-router-dom';
+import { useNavigate, Link } from 'react-router-dom';
+import { useAuth } from "../contexts/AuthContext";
 import './SignIn.css';
 
 const SignIn = () => {
-    const [username, setUsername] = useState('');
-    const [password, setPassword] = useState('');
+  const { login } = useAuth();
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+  const navigate = useNavigate();
 
-    const handleSubmit = (event) => {
-        event.preventDefault();
+  const handleUsernameChange = (event) => {
+    setUsername(event.target.value);
+  };
 
-        if (username && password) {
-            alert('Sign In successful!');
-        } else {
-            alert('Please enter both username and password!');
-        }
-    };
+  const handlePasswordChange = (event) => {
+    setPassword(event.target.value);
+  };
 
-    return (
-        <div className="SignIn">
-          <h1 className="OtterCollab">OtterCollab</h1>
-          <form onSubmit={handleSubmit}>
-            <div className="email-box">
-              <label htmlFor="emailInput">Email: </label>
-              <input
-                id="emailInput"
-                type="text"
-                placeholder="Email"
-              />
-            </div>
-            <div>
-              <label htmlFor="passwordInput">Password: </label>
-              <input
-                id="passwordInput"
-                type="password"
-                placeholder="Password"
-              />
-            </div>
-            <div>
-              <button type="submit" className="action-button">
-                Sign In
-              </button>
-            </div>
-          </form>
-          <p>
-            Not on OtterCollab? <Link to="/NewUser">Create Account</Link>
-          </p>
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    if (username && password) {
+      try {
+        await login(username, password);
+        navigate("/");
+      } catch (error) {
+        alert('Failed to sign in');
+        console.error(error);
+      }
+    } else {
+      alert('Please enter both username and password!');
+    }
+  };
+
+  return (
+    <div className="SignIn">
+      <h1 className="OtterCollab">OtterCollab</h1>
+      <form onSubmit={handleSubmit}>
+        <div className="username-box">
+          <label htmlFor="usernameInput">Username: </label>
+          <input
+            id="usernameInput"
+            type="text"
+            value={username}
+            onChange={handleUsernameChange}
+            placeholder="Username"
+          />
         </div>
-      );
+        <div>
+          <label htmlFor="passwordInput">Password: </label>
+          <input
+            id="passwordInput"
+            type="password"
+            value={password}
+            onChange={handlePasswordChange}
+            placeholder="Password"
+          />
+        </div>
+        <div>
+          <button type="submit" className="action-button">
+            Sign In
+          </button>
+        </div>
+      </form>
+      <p>
+        Not on OtterCollab? <Link to="/">Create Account</Link>
+      </p>
+    </div>
+  );
 };
 
 export default SignIn;
