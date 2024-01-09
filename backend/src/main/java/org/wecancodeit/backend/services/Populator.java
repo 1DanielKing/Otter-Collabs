f12af6518bc8271e948495e8980c88d55d64cb25
Omar Bonnet <omar.bonnet@gmail.com>
2024-01-09 10:04:45 -0500

    fixed image pathing for populator

--- backend/src/main/java/org/wecancodeit/backend/services/Populator.java
+++ backend/src/main/java/org/wecancodeit/backend/services/Populator.java
@@ -18,17 +18,17 @@ public class Populator implements CommandLineRunner {
         @Override
         public void run(String... args) throws Exception {
                 User user1 = new User("user1", "password1", "user1@example.com", "Guitar", "Rock",
-                                ExperienceLevelEnum.INTERMEDIATE, "image1.jpg");
+                                ExperienceLevelEnum.INTERMEDIATE, "/media/pictures/default-pfp/Otter1.png");
                 user1.addMusicTag("Tag1");
                 user1.addMusicTag("Tag2");
 
                 User user2 = new User("user2", "password2", "user2@example.com", "Piano", "Classical",
-                                ExperienceLevelEnum.ADVANCED, "image2.jpg");
+                                ExperienceLevelEnum.ADVANCED, "/media/pictures/default-pfp/Otter2.png");
                 user2.addMusicTag("Tag3");
                 user2.addMusicTag("Tag4");
 
                 User user3 = new User("user3", "password3", "user3@example.com", "Drums", "Pop",
-                                ExperienceLevelEnum.BEGINNER, "image3.jpg");
+                                ExperienceLevelEnum.BEGINNER, "/media/pictures/default-pfp/Otter3.png");
                 user3.addMusicTag("Tag5");
                 user3.addMusicTag("Tag6");
 
