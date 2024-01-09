b89359da8222829a86dcccd0ea6aabdd372da7cc
Wendy Gonzalez <wgonzalez1103@gmail.com>
2024-01-09 09:54:37 -0500

    populator

--- backend/src/main/java/org/wecancodeit/backend/services/Populator.java
+++ backend/src/main/java/org/wecancodeit/backend/services/Populator.java
@@ -1,39 +1,30 @@
 package org.wecancodeit.backend.services;
 
-import org.springframework.boot.CommandLineRunner;
-import org.springframework.stereotype.Component;
 import org.wecancodeit.backend.enums.ExperienceLevelEnum;
 import org.wecancodeit.backend.models.User;
-import org.wecancodeit.backend.repositories.UserRepository;
 
-@Component
-public class Populator implements CommandLineRunner {
+public class Populator {
+        public static void main(String[] args) {
 
-        private final UserRepository userRepository;
-
-        public Populator(UserRepository userRepository) {
-                this.userRepository = userRepository;
-        }
-
-        @Override
-        public void run(String... args) throws Exception {
+                // Create a few test users
                 User user1 = new User("user1", "password1", "user1@example.com", "Guitar", "Rock",
-                                ExperienceLevelEnum.INTERMEDIATE, "/media/pictures/default-pfp/Otter1.png");
+                                ExperienceLevelEnum.INTERMEDIATE, "image1.jpg");
                 user1.addMusicTag("Tag1");
                 user1.addMusicTag("Tag2");
 
                 User user2 = new User("user2", "password2", "user2@example.com", "Piano", "Classical",
-                                ExperienceLevelEnum.ADVANCED, "/media/pictures/default-pfp/Otter2.png");
+                                ExperienceLevelEnum.ADVANCED, "image2.jpg");
                 user2.addMusicTag("Tag3");
                 user2.addMusicTag("Tag4");
 
                 User user3 = new User("user3", "password3", "user3@example.com", "Drums", "Pop",
-                                ExperienceLevelEnum.BEGINNER, "/media/pictures/default-pfp/Otter3.png");
+                                ExperienceLevelEnum.BEGINNER, "image3.jpg");
                 user3.addMusicTag("Tag5");
                 user3.addMusicTag("Tag6");
 
-                userRepository.save(user1);
-                userRepository.save(user2);
-                userRepository.save(user3);
+                // Print the populated users for testing
+                System.out.println(user1);
+                System.out.println(user2);
+                System.out.println(user3);
         }
 }
