5ae273fd3f6a1d5f05211917619ed1433a4b4b30
Nathan Mange <nmange95@gmail.com>
2024-01-10 14:12:23 -0500

    works but needs improving.

--- frontend/src/pages/FindUsers.jsx
+++ frontend/src/pages/FindUsers.jsx
@@ -37,7 +37,6 @@ const FindUsers = () => {
         <button onClick={handleSearch}>Search</button>
       </div>
       <div>
-        {/* Render the FindUser component with props */}
         <FindUser searchInput={searchInput} selectedOption={selectedOption} />
       </div>
     </div>
