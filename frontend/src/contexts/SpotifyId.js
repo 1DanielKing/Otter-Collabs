4845cbc71d59739644af0c29dbb2ca04bd3a8c3b
Nathan Mange <nmange95@gmail.com>
2024-01-17 14:11:25 -0500

    spotifyAuth

---
+++ frontend/src/contexts/SpotifyId.js
@@ -0,0 +1,11 @@
+export const redirect_Uri = () => {
+  return "http://localhost:3000";
+};
+
+export const client_Secret = () => {
+  return "3a9ca12344df4079aee9501f9de56b8d";
+};
+
+export const client_Id = () => {
+  return "fcc6b41963c14be097b864c3bcf9a0fb";
+};
--- frontend/src/pages/ProfilePage.jsx
+++ frontend/src/pages/ProfilePage.jsx
@@ -1,12 +1,15 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { useAuth } from "../contexts/AuthContext";
 import DisplayProfile from "../components/DisplayProfile";
-
+import { SpotifyAuth, HandleCallback } from "../contexts/SpotifyAuth";
+import { useLocation, useNavigate } from "react-router-dom";
 import "./ProfilePage.css";
-
+import { client_Secret, client_Id, redirect_Uri } from "../contexts/SpotifyId";
 import EditProfileForm from "../components/EditProfileForm";
 
 const ProfilePage = () => {
+  const navigate = useNavigate();
+  const location = useLocation();
   const { user } = useAuth();
   const [editMode, setEditMode] = useState(false);
 
@@ -14,18 +17,31 @@ const ProfilePage = () => {
     setEditMode(!editMode);
   };
 
+  useEffect(() => {
+    if (location.pathname.includes("/") && location.search.includes("?code=")) {
+      HandleCallback(location, navigate);
+    }
+  }, [location]);
 
   if (!user) {
     console.log(user);
     return <div>Loading profile...</div>;
   }
+
   return (
     <div className="main-container">
       <div className="item-box">
         {editMode ? (
           <EditProfileForm user={user} toggleEditMode={toggleEditMode} />
         ) : (
-          <DisplayProfile data={user} toggleEditMode={toggleEditMode} />
+          <>
+            <DisplayProfile data={user} toggleEditMode={toggleEditMode} />
+            {location.pathname.includes("/") &&
+            location.search.includes("?code=") ? null : (
+              <SpotifyAuth />
+            )}{" "}
+            {/* Add condition to render SpotifyAuth */}
+          </>
         )}
       </div>
     </div>
@@ -33,4 +49,3 @@ const ProfilePage = () => {
 };
 
 export default ProfilePage;
-
