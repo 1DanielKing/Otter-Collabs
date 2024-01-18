2d7d0ea4d3e9b18c6fd31c272e38b2e95c2720bc
Omar Bonnet <omar.bonnet@gmail.com>
2024-01-18 17:35:34 -0500

    Refactor AudioHandler component

--- frontend/src/components/AudioHandler.js
+++ frontend/src/components/AudioHandler.js
@@ -1,10 +1,9 @@
 import React, { useState } from 'react';
-import axios from 'axios';
 import { useAuth } from '../contexts/AuthContext';
 
 
 const AudioHandler = ({ onFileChange }) => {
-    const { user } = useAuth();
+  const { user } = useAuth();
   const [audioFile, setAudioFile] = useState(null);
 
   // Function to handle file input change
@@ -13,37 +12,15 @@ const AudioHandler = ({ onFileChange }) => {
     setAudioFile(file);
 
     if (onFileChange) {
-        onFileChange(file);
-      }
-    // send the file to your backend using Axios
-    try {
-        const formData = new FormData();
-        formData.append('file', file);
-
-        const response = await axios.post('http://localhost:8080/api/audio/upload', formData, {
-            headers: {
-                'Authorization': `Bearer ${user.token}`,
-                'Content-Type': 'multipart/form-data',
-            },
-        });
-
-        if (response.status === 200) {
-            // Handle success
-            console.log('Audio file uploaded successfully');
-        } else {
-            // Handle error
-            console.error('Failed to upload audio file');
-        }
-    } catch (error) {
-        console.error('Error uploading audio file:', error.message);
+      onFileChange(file);
     }
-};
+  };
 
   return (
     <div>
-    <p>User ID: {user ? user.id : 'Not authenticated'}</p>
+      <p>User ID: {user ? user.id : 'Not authenticated'}</p>
       <input type="file" accept="audio/*" onChange={handleFileChange} />
-   {/* Display the selected audio file name */}
+      {/* Display the selected audio file name */}
       {audioFile && (
         <div>
           <h3>Selected Audio</h3>
