7fd99ab426c4b012a5280e3dd367f681cb454878
Omar Bonnet <omar.bonnet@gmail.com>
2024-01-22 10:00:30 -0500

    removed spotify stuff

--- frontend/src/pages/ProfilePage.jsx
+++ frontend/src/pages/ProfilePage.jsx
@@ -1,27 +1,17 @@
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 import { useAuth } from "../contexts/AuthContext";
 import DisplayProfile from "../components/DisplayProfile";
-import { SpotifyAuth, HandleCallback } from "../contexts/SpotifyAuth";
-import { useLocation, useNavigate } from "react-router-dom";
 import "./ProfilePage.css";
 import EditProfileForm from "../components/EditProfileForm";
 import MusicTagsDropdown from "../components/MusicTagsDropdown";
 
 const ProfilePage = () => {
-  const navigate = useNavigate();
-  const location = useLocation();
   const { user, loadProfileData } = useAuth();
   const [editMode, setEditMode] = useState(false);
 
   const toggleEditMode = () => {
     setEditMode(!editMode);
   };
-
-  useEffect(() => {
-    if (location.pathname.includes("/") && location.search.includes("?code=")) {
-      HandleCallback(location, navigate);
-    }
-  }, [location, navigate]);
 
   if (!user) {
     console.log(user);
